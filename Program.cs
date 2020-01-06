d7b3cab3efba336111bddc0b530832a2b99cd844
lexneville <jodyrichardson45@hotmail.com>
2020-01-06 12:28:10 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Hello World!");
+            /*  
+                Operators in C#
+                Arithmetic:  +, -, %, *, /, ++, --
+                Relational:  ==, !=, ===, !==, <, >, <=, >=
+                Bitwise:     &, |, ^ XOR, ~, <<, >>
+                Logical:     &&, ||, !
+                Assignment:  =, +=, -=, *=, /=, %=, &=, ^=, |=, <<=, >>=
+            */
+            int age = 40;
+            int realAge = 55;
+
+            if (age == 30 || realAge == 55) {
+                System.Console.WriteLine("True");
+            } else {
+                System.Console.WriteLine("False");
+            }
+
+            Console.WriteLine();
+
+        }
+    }
+}
